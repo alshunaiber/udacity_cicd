6c0f7d351bc797b38312e6c5d1ef541fe50f0dbd
Fahad Alshunaiber <fahad@Fahads-MacBook-Pro.local>
2020-11-14 13:14:13 +0300

    docker run script

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t udacity-project:1.0 .
 
 # Step 2: 
 # List docker images
+docker images 
 
 # Step 3: 
 # Run flask app
+
+docker run -d --name udacity-project -p 8080:80 udacity-project:1.0
