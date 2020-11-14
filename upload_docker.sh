c93b4b0a1bc04114a0cacf374212ed96972f4635
Fahad Alshunaiber <fahad@Fahads-MacBook-Pro.local>
2020-11-14 14:25:24 +0300

    finished upload docker script

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=faalsh/udacity-project:1.0
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username=faalsh
+docker tag udacity-project:1.0 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
